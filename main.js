1819130ecbff89b46015f747a1c2e29b485b59c3
luthfi.hakim <luthfihakim2004@mail.ugm.ac.id>
2025-05-27 23:18:08 +0700

    icustom

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ import { client } from './client.js';
 import { handleMessage } from './handlers/msgHandler.js';
 
 client.once('ready', () => {
+  client.user.setPresence({
+    activities: [{
+      name: 'ur ass 👀',
+      type: Discord.ActivityType.Watching,
+    }],
+    status: 'online',
+  });
   console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
