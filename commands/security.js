3aa2fb90c4dae97a6f7ddec872082664f17ee8a1
luthfi.hakim <luthfihakim2004@mail.ugm.ac.id>
2025-06-01 22:25:31 +0700

    beta menu command

---
+++ commands/security.js
@@ -0,0 +1,25 @@
+const { StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('discord.js');
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('security')
+    .setDescription('Show menu of security features'),
+  async execute(interaction) {
+		const select = new StringSelectMenuBuilder()
+			.setCustomId('starter')
+			.setPlaceholder('Make a selection!')
+			.addOptions(
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Bulbasaur')
+					.setDescription('The dual-type Grass/Poison Seed Pokémon.')
+					.setValue('bulbasaur'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Charmander')
+					.setDescription('The Fire-type Lizard Pokémon.')
+					.setValue('charmander'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Squirtle')
+					.setDescription('The Water-type Tiny Turtle Pokémon.')
+					.setValue('squirtle'),
+			);  }
+};
